211840f3651d303c23c940eb657d0f2b24aa9179
OriToren <oritoren65@gmail.com>
2025-08-02 17:14:56 +0300

    Update cleanup.sh

--- Scripts/cleanup.sh
+++ Scripts/cleanup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-echo "=== Joomla Cleanup (WARNING: DESTRUCTIVE!) ==="
-
+echo "=== Joomla Cleanup==="
 read -p "This will DELETE all containers and data! Continue? (y/n): " -n 1 -r
 echo
-
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   docker stop joomla joomla-db
   docker rm joomla joomla-db
